fe02b69b0b1af793f380e69be86b42901be24229
Narmina <naraibr22@gmail.com>
2022-12-24 14:51:31 +0400

    working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import MovieList from './components/MovieList'
 import MovieListHeading from './components/MovieListHeading'
+import SearchBox from './components/SearchBox'
 
 const App = () => {
     const [movies, setMovies] = useState([])
@@ -22,8 +23,9 @@ const App = () => {
     }, [])
     return (
     <div className='container-fluid movie-app'>
-        <div className='row'>
+        <div className='row d-flex align-items-center mt-4 mb-4'>
             <MovieListHeading heading="Movies" />
+            <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
         </div>
         <div className='row'>
         <MovieList movies={movies} />
