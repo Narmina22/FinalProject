89bbf771fb6f56bff4122eaf12709a25d4dbf94d
Narmina <naraibr22@gmail.com>
2022-12-24 15:39:37 +0400

    styles, add to favourites

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import './App.css'
 import MovieList from './components/MovieList'
 import MovieListHeading from './components/MovieListHeading'
 import SearchBox from './components/SearchBox'
+import AddFavourites from './components/AddFavourites'
 
 const App = () => {
     const [movies, setMovies] = useState([])
+    const [favourites, setFavourites] = useState([])
     const [searchValue, setSearchValue] = useState('')
 
-    const getMovieRequest = async () => {
-        const url = "http://www.omdbapi.com/?s=spider-man&apikey=a83618cf"
+    const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a83618cf`
 
         const response = await fetch(url)
         const responseJson = await response.json()
-        console.log(responseJson)
-        setMovies(responseJson.Search)
+
+        if(responseJson.Search) {
+            setMovies(responseJson.Search)
+        }
     }
 
     useEffect(()=> {
-        getMovieRequest()
-    }, [])
+        getMovieRequest(searchValue)
+    }, [searchValue])
+
+    const addFavouriteMovie = (movie) => {
+        const newFavouriteList = [...favourites, movie]
+        setFavourites(newFavouriteList)
+    }
     return (
     <div className='container-fluid movie-app'>
         <div className='row d-flex align-items-center mt-4 mb-4'>
@@ -28,7 +37,7 @@ const App = () => {
             <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
         </div>
         <div className='row'>
-        <MovieList movies={movies} />
+        <MovieList movies={movies} handleFavouritesClick={addFavouriteMovie} favouriteComponent = {AddFavourites}/>
         </div>
     </div>
     )
