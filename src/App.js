f26b5188695bc3079342485f00c4f2464acf6ffe
Narmina <naraibr22@gmail.com>
2022-12-25 17:16:00 +0400

    added adaptation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieList from './components/MovieList'
 import MovieListHeading from './components/MovieListHeading'
 import SearchBox from './components/SearchBox'
 import AddFavourites from './components/AddFavourites'
+import RemoveFavourites from './components/RemoveFavourites'
 
 const App = () => {
     const [movies, setMovies] = useState([])
@@ -30,14 +31,33 @@ const App = () => {
         const newFavouriteList = [...favourites, movie]
         setFavourites(newFavouriteList)
     }
+
+    const removeFavouriteMovie = (movie) => {
+        const newFavouriteList = favourites.filter((favourite)=> favourite.imdbID !== movie.imdbID)
+        setFavourites(newFavouriteList)
+    } 
     return (
     <div className='container-fluid movie-app'>
         <div className='row d-flex align-items-center mt-4 mb-4'>
             <MovieListHeading heading="Movies" />
             <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
         </div>
+        <div className='content row'>
+            <MovieList 
+            movies={movies} 
+            handleFavouritesClick={addFavouriteMovie} 
+            favouriteComponent = {AddFavourites}
+            />
+        </div>
+        <div className='row d-flex align-items-center mt-4 mb-4'>
+            <MovieListHeading heading="Favourites" />
+        </div>
         <div className='row'>
-        <MovieList movies={movies} handleFavouritesClick={addFavouriteMovie} favouriteComponent = {AddFavourites}/>
+            <MovieList 
+            movies={favourites} 
+            handleFavouritesClick={removeFavouriteMovie} 
+            favouriteComponent = {RemoveFavourites}
+            />
         </div>
     </div>
     )
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,9 +5,9 @@ const MovieList = (props) => {
     return (
         <>
         {props.movies.map((movie, index)=> 
-        <div className='image-container d-flex justify-content-start m-3'>
+        <div className='image-container d-flex justify-content-start'>
             <img src={movie.Poster} alt='movie'></img>
-            <div onClick={props.handleFavouritesClick} className='overlay d-flex align-items-center justify-content-center'>
+            <div onClick={ ()=> props.handleFavouritesClick(movie)} className='overlay d-flex align-items-center justify-content-center'>
                 <FavouriteComponent />
             </div>
         </div>)}
